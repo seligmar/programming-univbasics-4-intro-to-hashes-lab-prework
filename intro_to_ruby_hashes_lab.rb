cd00f4315ba8e71b35fce17105b65ba29d17f4b7
Mary Selig <seligmar@gmail.com>
2019-06-19 15:54:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,6 +25,12 @@ end
 def update_counting_hash(hash, key)
     if hash[key] 
       hash[key] += 1 
-      else hash.fetch(key, 1)   
+      else hash[key] = 1   
 end
 end 
+
+if shipping_manifest["top hat"]
+  shipping_manifest["top hat"] += 1
+else
+  shipping_manifest["top hat"] = 1
+end
